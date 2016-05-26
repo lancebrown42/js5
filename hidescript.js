cd03946c29b21020e92ae6f8f5d0321aca95aa3d
Lance <Lance@Lances-Macbook-Air.local>
2016-05-26 11:29:44 -0600

    oh god it finally works

--- hidescript.js
+++ hidescript.js
@@ -1,29 +1,52 @@
-var yourName = document.getElementById('name')
-var food = document.querySelector('p.food')
-var hobby = document.querySelector('p.hobby')
-var like = document.querySelector('p.like')
-var dislike = document.querySelector('p.dislike')
+var yourName = document.querySelector('h2')
+var food = document.querySelector('.food p')
+var hobby = document.querySelector('.hobby p')
+var like = document.querySelector('.like p')
+var dislike = document.querySelector('.dislike p')
 // var nameInput = document.getElementsByName('nameInput')
 // var foodInput = document.getElementsByName('foodInput')
 // var hobbyInput = document.getElementsByName('hobbyInput')
 // var likeInput = document.getElementsByName('likeInput')
 // var dislikeInput = document.getElementsByName('dislikeInput')
 function makeInput(text){
-	// console.log(input)
-	text.removeEventListener('click',function(){makeInput(text)})
-	// var boxHeight = getHeight(text)
-	text.innerHTML = '<input type = \'text\' name = \''+text+'Input\'></input>'
-	document.querySelector('input').addEventListener('blur', function(){makeBlur(text)})
-
 	// input.style.height = getHeight(text)+'px'
+	return function() {
+		console.log(text)
+		var type = text.tagName
+		console.log(type)
+		text.removeEventListener('click',makeInput(text))
+		// var boxHeight = getHeight(text)
+		text.innerHTML = '<input type = \'text\' name = \''+text+'Input\'></input>'
+		document.querySelector('input').focus()
+		if (type == 'H2') {
+			document.querySelector('input').addEventListener('blur',function(){makeBlurName(text)})
+		}
+		else{
+			document.querySelector('input').addEventListener('blur', function(){makeBlurP(text)})
+		}
 
+	}
+
+}
+function makeBlurP(text){
+	text.innerHTML = '<p>'+(document.querySelector('input').value || 'Edit...')+'</p>'
+	text.addEventListener('click',function(){makeInput(text)})
+}
+function makeBlurName(text){
+	text.innerHTML = '<h2>'+(document.querySelector('input').value || 'Name...')+'</h2>'
+	text.addEventListener('click',function(){makeInput(text)})
 }
 // function serveName(){
 // 	makeInput(yourName, nameInput)
 // }
 // console.log(yourName)
 // console.log(nameInput)
-yourName.addEventListener('click',function(){makeInput(yourName)})
+console.log(food)
+yourName.addEventListener('click', makeInput(yourName))
+food.addEventListener('click', makeInput(food))
+hobby.addEventListener('click', makeInput(hobby))
+like.addEventListener('click', makeInput(like))
+dislike.addEventListener('click', makeInput(dislike))
 function getHeight(element){
 	// console.log(element.scrollHeight)
 	return element.scrollHeight
